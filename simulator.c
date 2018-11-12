67a34e668849e4b7fbc4057d917a4afb52358770
krimson8 <krimson8@gmail.com>
2018-11-13 01:49:59 +0800

    Reshape structure

--- simulator.c
+++ simulator.c
@@ -7,16 +7,27 @@ typedef struct{
     long long content;
 }block;
 
-block ***cache_init(int cache_size, int assoc, int block_size) {
+block **cache_init(int cache_size, int assoc, int block_size) {
     int set_number = cache_size / assoc / block_size;
 
-    block ***cache = malloc(set_number * sizeof(block));
+    block **cache = (block **)malloc(set_number * sizeof(block*));
 
     for(int i = 0; i < set_number; i++) {
-        cache[i] = malloc(assoc * sizeof(block));
+        cache[i] = (block *)malloc(assoc * sizeof(block));
+    }
+
+    long long k = 1;
+    for(int i = 0; i < set_number; i++) {
         for(int j = 0; j < assoc; j++) {
-            cache[i][j] = malloc(sizeof(block));
+            cache[i][j].content = k++;
         }
+    }
+
+    for(int i = 0; i < set_number; i++) {
+        for(int j = 0; j < assoc; j++) {
+            printf("%lld ", cache[i][j].content);
+        }
+        printf("\n");
     }
     return cache;
 }
@@ -32,5 +43,5 @@ int main(int argc, char *argv[]) {
     int block_size = atoi(argv[3]);
     int op_type = atoi(argv[4]);
 
-    block ***cache = cache_init(cache_size, assoc, block_size);
+    block **cache = cache_init(cache_size, assoc, block_size);
 }
